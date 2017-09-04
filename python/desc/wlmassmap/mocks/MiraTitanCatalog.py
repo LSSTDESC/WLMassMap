b2c4a199af19a7947d84216c1648dca1c1eebd35
Francois <fr.eiffel@gmail.com>
2017-09-04 14:07:32 -0400

    Added config file for mira-titan sims

---
+++ python/desc/wlmassmap/mocks/MiraTitanCatalog.py
@@ -0,0 +1,23 @@
+from astropy.table import Table
+from GCR import BaseGalaxyCatalog
+
+__all__ = ['MiraTitanCatalog']
+
+class MiraTitanCatalog(BaseGalaxyCatalog):
+    """
+    Mira Titan catalog reader for the DESCQA generic catalog reader
+    """
+
+    def _subclass_init(self, filename, base_catalog_dir=os.curdir, **kwargs):
+
+        self._quantity_modifiers ={
+            'ra_true': 'ra_arcmin',
+            'dec_true': 'dec_arcmin',
+            'redshift_true': 'z_spec',
+            'shear_1': 'shear1',
+            'shear_2': 'shear2'
+        }
+
+    @staticmethod
+    def _fetch_native_quantity(dataset, native_quantity):
+        return dataset[native_quantity].value
