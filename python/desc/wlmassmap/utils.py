3f8b49b670b56b21fe66fc050431fe340c0caf6f
Francois <fr.eiffel@gmail.com>
2017-09-04 20:40:39 -0400

    removed useless file

--- python/desc/wlmassmap/utils.py
+++
@@ -1,9 +0,0 @@
-# Module contains a number of utility functions
-
-
-def rotate_catalog(catalog, new_center):
-    """
-    Applies a rotation to a shape catalog, which will be centered at the provided
-    coordinates.
-    """
-    raise NotImplementedError("TODO: implement shape rotations")
