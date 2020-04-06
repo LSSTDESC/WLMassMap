546a588e84d0db64644d4af854d2a5e5bde72207
EiffL <fr.eiffel@gmail.com>
2020-04-06 17:51:41 +0200

    Adds draft of txpipe compatible convergence maps

--- python/mmpipe/apps.py
+++ python/mmpipe/apps.py
@@ -81,5 +81,84 @@ class convergenceMapPipe(PipelineStage):
                                 'zero_padding':config['zero_padding']}
         convergence_map(config)
 
+class TXConvergenceMaps(PipelineStage):
+    name = 'TXConvergenceMaps'
+    inputs = [
+        ('diagnostic_maps', DiaognisticMaps)
+    ]
+    outputs = [
+        ('mass_maps', DiagnosticMaps)
+    ]
+    config_options = {
+        'smoothing': 5.,
+        'algorithm': 'spherical_ks',
+        'flip_g1': False,
+        'flip_g2': False,
+        'zero_padding': 128,
+        'lmax': 4096
+    }
+
+    def run(self):
+        from desc.wlmassmap.kaiser_squires import flat_KS_map, healpix_KS_map
+
+        config = self.config
+        shear_maps, weights = self.load_maps()
+
+        if config['algorithm'] == 'flat_ks':
+            kappa_e, kappa_b = flat_KS_map(gmap)
+
+        elif config['algorithm'] == 'healpix_ks':
+            kappa_e, kappa_b = healpix_KS_map(gmap, lmax=config['lmax'])
+        else:
+            raise NotImplementedError
+
+
+    def load_maps():
+        import healpy
+
+        # Load the various input maps and their metadata
+        map_file = self.open_input('diagnostic_maps', wrapper=True)
+        pix_info = map_file.read_map_info('mask')
+        area = map_file.file['maps'].attrs["area"]
+
+        nbin_source = map_file.file['maps'].attrs['nbin_source']
+
+        # Choose pixelization and read mask and systematics maps
+        pixel_scheme = choose_pixelization(**pix_info)
+
+        # Load the mask. It should automatically be the same shape as the
+        # others, based on how it was originally generated.
+        # We remove any pixels that are at or below our threshold (default=0)
+        mask = map_file.read_map('mask')
+        mask[np.isnan(mask)] = 0
+        mask[mask==healpy.UNSEEN] = 0
+
+        # Load all the maps in.
+        # TODO: make it possible to just do a subset of these
+        ngal_maps = [map_file.read_map(f'ngal_{b}') for b in range(nbin_lens)]
+        g1_maps = [map_file.read_map(f'g1_{b}') for b in range(nbin_source)]
+        g2_maps = [map_file.read_map(f'g2_{b}') for b in range(nbin_source)]
+        lensing_weights = [map_file.read_map(f'lensing_weight_{b}') for b in range(nbin_source)]
+
+        # Mask any pixels which have the healpix bad value
+        for (g1, g2, lw) in zip(g1_maps, g2_maps, lensing_weights):
+            lw[g1 == healpy.UNSEEN] = 0
+            lw[g2 == healpy.UNSEEN] = 0
+
+        # When running on the CosmoDC2 mocks I've found I need to flip
+        # both g1 and g2 in order to get both positive galaxy-galaxy lensing
+        # and shear-shear spectra.
+        if self.config['flip_g1']:
+            for g1 in g1_maps:
+                w = np.where(g1!=healpy.UNSEEN)
+                g1[w]*=-1
+
+        if self.config['flip_g2']:
+            for g2 in g2_maps:
+                w = np.where(g2!=healpy.UNSEEN)
+                g2[w]*=-1
+
+        return [np.stack([g1,g2], axis=0) for (g1,g2) in zip(g1_maps, g2_maps)], lensing_weights
+
 if __name__ == '__main__':
     cls = PipelineStage.main()
